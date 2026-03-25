6c07eba17d99e577f968e373c761b678d5538344
yeshu1012 <yashaxxxxxxxx@gmail.com>
2026-03-25 13:06:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('HealthConnect Ultra Running');
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("HealthConnect Ultra Running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
